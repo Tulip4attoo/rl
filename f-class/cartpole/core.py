0ae63fd3191e8cd5fb6d1e444abdff62a0ff1afb
Linh <huylinh.nguyen@outlook.com>
2018-11-01 13:57:34 +0700

    prune the code

--- f-class/cartpole/core.py
+++ f-class/cartpole/core.py
@@ -3,6 +3,7 @@ import gym
 import time
 import tensorflow as tf
 from collections import deque # Ordered collection with ends
+from model import DQNetwork
 
 import warnings # This ignore all the warning messages that are normally printed during the training because of skiimage
 warnings.filterwarnings('ignore')
@@ -31,59 +32,7 @@ discount_rate = 0.95               # Discounting rate
 pretrain_length = batch_size   # Number of experiences stored in the Memory when initialized for the first time
 memory_size = 1000000          # Number of experiences the Memory can keep
 
-### MODIFY THIS TO FALSE IF YOU JUST WANT TO SEE THE TRAINED AGENT
-training = True
-
-## TURN THIS TO TRUE IF YOU WANT TO RENDER THE ENVIRONMENT
-episode_render = False
-
-class DQNetwork:
-    def __init__(self, state_size, action_size, learning_rate, name='DQNetwork'):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.learning_rate = learning_rate
-
-        with tf.variable_scope(name):
-            # We create the placeholders
-            self.inputs_ = tf.placeholder(tf.float32, [None, state_size], name="inputs")
-            self.actions_ = tf.placeholder(tf.float32, [None, 2], name="actions_")
-
-            self.dense1 = tf.layers.dense(inputs = self.inputs_,
-                                    units = 4,
-                                    activation = tf.nn.relu,
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                    name="dense1")
-            self.dense2 = tf.layers.dense(inputs = self.dense1,
-                                    units = 20,
-                                    activation = tf.nn.relu,
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                    name="dense2")
-            self.dense3 = tf.layers.dense(inputs = self.dense2,
-                                    units = 10,
-                                    activation = tf.nn.relu,
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                    name="dense3")
-            self.output = tf.layers.dense(inputs = self.dense3, 
-                                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                    units = 2, 
-                                    activation=None)
-
-            # Remember that target_Q is the R(s,a) + ymax Qhat(s', a')
-            self.target_Q = tf.placeholder(tf.float32, [None], name="target")
-
-            # Q is our predicted Q value.
-            self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
-            # The loss is the difference between our predicted Q_values and the Q_target
-            # Sum(Qtarget - Q)^2
-            self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
-            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
-
-# Reset the graph
-tf.reset_default_graph()
-
-# Instantiate the DQNetwork
 DQNetwork = DQNetwork(state_size, action_size, learning_rate)
-
 
 # PART II: GEN MEMORY
 print("gen memory")
@@ -120,23 +69,13 @@ This function will do the part
 With ϵ select a random action atat, otherwise select at=argmaxaQ(st,a)
 """
 def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
-    ## EPSILON GREEDY STRATEGY
-    # Choose action a from state s using epsilon greedy.
-    ## First we randomize a number
     exp_exp_tradeoff = np.random.rand()
-
-    # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
     explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
-    
     if (explore_probability > exp_exp_tradeoff):
         # Make a random action (exploration)
         action = np.random.randint(0, action_size)
     else:
-        # Get action from Q-network (exploitation)
-        # Estimate the Qs values state
         Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state.reshape((1, *state.shape))})
-        
-        # Take the biggest Q value (= the best action)
         choice = np.argmax(Qs)
         action = possible_actions[int(choice)]
     return action, explore_probability
@@ -144,83 +83,75 @@ def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, a
 
 # PART III: TRAIN AGENT
 
+tf.reset_default_graph()
+
 print("train agent")
 # Saver will help us to save our model
 saver = tf.train.Saver()
 
-if training == True:
-    with tf.Session() as sess:
-        # Initialize the variables
-        sess.run(tf.global_variables_initializer())
-        
-        # Initialize the decay rate (that will use to reduce epsilon) 
-        decay_step = 0
+with tf.Session() as sess:
+    # Initialize the variables
+    sess.run(tf.global_variables_initializer())
+    
+    # Initialize the decay rate (that will use to reduce epsilon) 
+    decay_step = 0
 
-        for episode in range(total_episodes):
-            step = 0
-            episode_rewards = []
-            state = env.reset()
+    for episode in range(total_episodes):
+        step = 0
+        episode_rewards = []
+        state = env.reset()
 
-            while step < max_steps:
-                step += 1
-                decay_step +=1
+        while step < max_steps:
+            step += 1
+            decay_step +=1
 
-                # Predict the action to take and take it
-                action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, possible_actions)
+            # Predict the action to take and take it
+            action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, possible_actions)
+            new_state, reward, done, _ = env.step(action)
+            episode_rewards.append(reward)
+            if done:
+                # Set step = max_steps to end the episode
+                step = max_steps
+                total_reward = np.sum(episode_rewards)
+                print('Episode: {}'.format(episode),
+                            'Total reward: {}'.format(total_reward),
+                            'Training loss: {:.4f}'.format(loss),
+                            'Explore P: {:.4f}'.format(explore_probability))
+            else:
                 new_state, reward, done, _ = env.step(action)
-                episode_rewards.append(reward)
-                if done:
-                    # Set step = max_steps to end the episode
-                    step = max_steps
-                    total_reward = np.sum(episode_rewards)
-                    print('Episode: {}'.format(episode),
-                              'Total reward: {}'.format(total_reward),
-                              'Training loss: {:.4f}'.format(loss),
-                              'Explore P: {:.4f}'.format(explore_probability))
+                memory.add((state, action, reward, new_state, done))
+                state = new_state
+
+            ### LEARNING PART            
+            # Obtain random mini-batch from memory
+            batch = memory.sample(batch_size)
+            states_mb = np.array([each[0] for each in batch])
+            actions_mb = np.array([each[1] for each in batch])
+            actions_mb = np.array([np.array((1,0)) if i == 0 else np.array((0,1)) for i in actions_mb])
+            rewards_mb = np.array([each[2] for each in batch]) 
+            next_states_mb = np.array([each[3] for each in batch])
+            dones_mb = np.array([each[4] for each in batch])
+
+            target_Qs_batch = []
+            # Get Q values for next_state 
+            Qs_next_state = sess.run(DQNetwork.output, \
+                    feed_dict = {DQNetwork.inputs_: next_states_mb})
+            
+            # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+            for i in range(0, len(batch)):
+                terminal = dones_mb[i]
+                # If we are in a terminal state, only equals reward
+                if terminal:
+                    target_Qs_batch.append(rewards_mb[i])
                 else:
-                    new_state, reward, done, _ = env.step(action)
-                    memory.add((state, action, reward, new_state, done))
-                    state = new_state
+                    target = rewards_mb[i] + discount_rate * np.max(Qs_next_state[i])
+                    target_Qs_batch.append(target)
+            targets_mb = np.array([each for each in target_Qs_batch])
+            loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+                                feed_dict={DQNetwork.inputs_: states_mb,
+                                            DQNetwork.target_Q: targets_mb,
+                                            DQNetwork.actions_: actions_mb})
 
-                ### LEARNING PART            
-                # Obtain random mini-batch from memory
-                batch = memory.sample(batch_size)
-                # states_mb = np.array([each[0] for each in batch], ndmin=3)
-                states_mb = np.array([each[0] for each in batch])
-                actions_mb = np.array([each[1] for each in batch])
-                actions_mb = np.array([np.array((1,0)) if i == 0 else np.array((0,1)) for i in actions_mb])
-                rewards_mb = np.array([each[2] for each in batch]) 
-                next_states_mb = np.array([each[3] for each in batch])
-                # next_states_mb = np.array([each[3] for each in batch], ndmin=3)
-                dones_mb = np.array([each[4] for each in batch])
-
-                target_Qs_batch = []
-
-                 # Get Q values for next_state 
-                Qs_next_state = sess.run(DQNetwork.output, \
-                        feed_dict = {DQNetwork.inputs_: next_states_mb})
-                
-                # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
-                for i in range(0, len(batch)):
-                    terminal = dones_mb[i]
-
-                    # If we are in a terminal state, only equals reward
-                    if terminal:
-                        target_Qs_batch.append(rewards_mb[i])
-                        
-                    else:
-                        target = rewards_mb[i] + discount_rate * np.max(Qs_next_state[i])
-                        target_Qs_batch.append(target)
-                        
-
-                targets_mb = np.array([each for each in target_Qs_batch])
-
-                loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
-                                    feed_dict={DQNetwork.inputs_: states_mb,
-                                               DQNetwork.target_Q: targets_mb,
-                                               DQNetwork.actions_: actions_mb})
-
-            # Save model every 5 episodes
-            if episode % 500 == 0:
-                save_path = saver.save(sess, "./models/model.ckpt")
-                print("Model Saved")
+        if episode % 500 == 0:
+            save_path = saver.save(sess, "./models/model.ckpt")
+            print("Model Saved")
